249a37df2b395256dd85f69981062017b5b3d751
Josh Ogden <77122008+J-Ogden99@users.noreply.github.com>
2023-08-23 15:24:32 -0600

    Adjust docs and rename stats with more readable names

--- CalcErrFromObserved.py
+++ CalcErrFromObserved.py
@@ -12,7 +12,10 @@ import HydroErr.HydroErr as Hs
 V1_DIR = 'geoglows1/outputs/'
 V2_DIR = 'geoglows2/outputs/'
 V2_LOOKUP_TBL = 'master_table.parquet'
+
+# Must match function names in HydroErr/Hydrostats
 STATS_LIST = ['me', 'rmse', 'kge', 'r_squared', 'nse', 'mape']
+# Used to rename stat columns
 STATS_NAMES = ['Mean Error', 'Root Mean Squared Error', 'Kling-Gupta Efficiency', 'R^2', 'Nash-Sutcliffe Efficiency',
                'Mean Absolute Percent Error']
 
@@ -29,9 +32,13 @@ def calc_error_metrics(gauge_data: pd.DataFrame, flow_col: str = 'Qout', **model
     Returns:
         pd.DataFrame: A DataFrame containing calculated error metrics for each model time series.
     """
+    # Add a column for each stat
     metrics_df = pd.DataFrame(columns=STATS_LIST)
+
+    # Add row for each provided model timeseries
     for model_name, model_df in model_timeseries.items():
         stats_dict = {}
+        # Calculate each stat using HydroErr and store in dictionary, then add as a row to the output dataframe
         for stat in STATS_LIST:
             stat_calc = getattr(Hs, stat)
             stat_val = stat_calc(model_df[flow_col], gauge_data[flow_col])
@@ -39,7 +46,8 @@ def calc_error_metrics(gauge_data: pd.DataFrame, flow_col: str = 'Qout', **model
         stats_df = pd.DataFrame(stats_dict, index=[model_name])
         metrics_df = pd.concat([metrics_df, stats_df])
 
-    return metrics_df
+    # Rename columns based on the longer names defined in STATS_NAMES
+    return metrics_df.rename(columns={short: long for short, long in zip(STATS_LIST, STATS_NAMES)})
 
 
 def get_vpu_nc(vpu_dir):
@@ -63,6 +71,7 @@ def calc_metrics_on_all_gauges(assigned_gauges: pd.DataFrame):
     Returns:
         pd.DataFrame: DataFrame containing calculated metrics for all gauges and models.
     """
+    # Prepare gauge assignments dataframe and lookup mechanisms to match ids to region and vpu codes
     gid_col = 'gauge_id'
     v1_id_col = 'geoglows_id'
     v2_id_col = 'nga_id'
@@ -70,7 +79,12 @@ def calc_metrics_on_all_gauges(assigned_gauges: pd.DataFrame):
     assigned_gauges['v1_region'] = assigned_gauges[v1_id_col].apply(reach_to_region)
     assigned_gauges['v2_vpu_code'] = assigned_gauges[v2_id_col].apply(
         lambda x: v2_lookup.loc[v2_lookup['TDXHydroLinkNo'] == x, 'VPUCode'].values[0])
+
+    # Sort by v1 region, then v2 vpu code, so that already opened timeseries can be reused if necessary for gauges in
+    # the same region
     assigned_gauges.sort_values(by=['v1_region', 'v2_vpu_code'], inplace=True)
+
+    # Make a copy in which to write the stats columns
     out_df = assigned_gauges.copy()
     old_v1_region, old_v2_vpu_code = ''
 
@@ -81,6 +95,7 @@ def calc_metrics_on_all_gauges(assigned_gauges: pd.DataFrame):
         v1_region = gauge_row['v1_region']
         v2_vpu_code = gauge_row['v2_vpu_code']
 
+        # Only read the historical simulation data if the gauge is in a different region/vpu than the last one
         if v1_region != old_v1_region:
             v1_hist_sim = get_vpu_nc(os.path.join(V1_DIR, v1_region)).sel(rivid=v1_id, nv=0) \
                 .to_dataframe().reset_index()
@@ -92,13 +107,14 @@ def calc_metrics_on_all_gauges(assigned_gauges: pd.DataFrame):
 
         gauge_timeseries = pd.read_csv(f'{gauge_id}.csv')
 
+        # Get metrics dataframe
         metrics = calc_error_metrics(gauge_timeseries, geoglows_v1=v1_hist_sim, geoglows_v2=v2_hist_sim)
 
-        # Reset the index and reshape the DataFrame
+        # Reset the index and reshape the DataFrame to be one row
         metrics = metrics.stack().to_frame().T
 
-        # Combine index and column names
-        metrics.columns = [f'{index}_{column}' for index, column in metrics.columns]
+        # Combine index and column names to get a column for each stat and for each model run
+        metrics.columns = [f'{index} - {column}' for index, column in metrics.columns]
 
         out_df.loc[gauge_row.index, metrics.columns] = metrics
 
